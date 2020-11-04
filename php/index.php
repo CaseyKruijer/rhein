44809f6d63bdfb285dee593ed7df9503de548b40
Kruijer Gamemaster <caseykruijer@gmail.com>
2020-11-04 07:51:28 +0100

    start

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<doctype>
+    <head>
+        <meta name="viewport"content="eidth=device-witdth, initial-scale=1-0">
+        <link rel="stylesheet" href="../css/main.css">
+    </head>
+    <body>
+        <h1 class="logo">Over de Rhein</h1>  
+        <div class="topnav">
+            <a href="#" class="navtext active">kabel test</a>
+            <a href="#" class="navtext">hijskraan gegevens</a>
+            <a href="#" class="navtext">hijskraan keuring</a>
+        </div>
+    </body>
